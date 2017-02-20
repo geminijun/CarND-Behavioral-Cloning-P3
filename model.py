6f628a5584ef69a38eae4f1292c6ac917a1f52ac
Jun Gan <geminijun@gmail.com>
2017-02-20 04:25:01 -0800

    NV model

--- model.py
+++ model.py
@@ -1,47 +1,44 @@
 import csv
 import cv2
-
-DATA_DIR = 'data/'
-lines = []
-with open(DATA_DIR + 'driving_log.csv', 'rb') as csvfile:
-  reader = csv.reader(csvfile, delimiter=' ', quotechar='|')
-  for line in reader:
-    lines.append(line)
-images = []
-measurements = []
-for line in lines:
-  for i in range(3):
-    source_path = line[i]
-    tokens = source_path.split('/')
-    filename = tokens[-1]
-    local_path = "./data/" + filename;
-    image = cv2.imread(local_path)
-    images.append(image)
-  correction = 0.2
-  measurement = float(line[3])
-  measurements.append(measurement)
-  measurements.append(measurement+correction)
-  measurements.append(measurement-correction)
-
-augmented_images = []
-augmented_measurements = []
-for image, measurement in zip(images, measurements):
-  augmented_measurements.append(measurement)
-  augmented_images.append(image)
-  flipped_image = cv2.flip(image, 1)
-  flipped_measurement = measurement * -1.0
-  augmented_images.append(flipped_image)
-  augmented_measurements.append(flipped_measurement)
-
 import numpy as np
 
-X_train = np.array(augmented_images)
-y_train = np.array(augmented_measurements)
+DATA_DIR = 'data/'
+IMG_DIR = 'IMG/'
+images = []
+measurements = []
+correction = 0.2
+with open(DATA_DIR + 'driving_log.csv', 'rt') as csvfile:
+  reader = csv.reader(csvfile, delimiter=',', quotechar='|')
+  for line in reader:
+    for i in range(3): # 0: center 1: left 2: right
+      source_path = line[i]
+      tokens = source_path.split('/')
+      filename = tokens[-1]
+      local_path = DATA_DIR + IMG_DIR + filename;
+ #     print(local_path)
+      image = cv2.imread(local_path)
+#      print(image)
+      images.append(image)
+      flipped_image = cv2.flip(image, 1)
+      images.append(flipped_image)
+#    print(images)
+#    exit();
+    measurement = float(line[3])
+    measurements.append(measurement)
+    measurements.append(measurement+correction) # left
+    measurements.append(measurement-correction) # right
+    flipped_measurement = measurement * -1.0
+    measurements.append(flipped_measurement)
+    measurements.append(flipped_measurement+correction)
+    measurements.append(flipped_measurement-correction)
+
+X_train = np.array(images)
+y_train = np.array(measurements)
+print(X_train.shape)
 
 # Python generator
 
 # 5k images
-
 
 import keras
 from keras.models import Sequential
@@ -52,16 +49,24 @@ from keras.layers.pooling import MaxPooling2D
 model = Sequential()
 model.add(Lambda(lambda x: x/255. - 0.5, input_shape=(160,320,3)))
 model.add(Cropping2D(cropping=((70, 25), (0,0))))
-model.add(Convolution2D(6,5,5,activation='relu'))
-model.add(MaxPooling2D())
-model.add(Convolution2D(16,5,5,activation='relu'))
-model.add(MaxPooling2D())
+model.add(Convolution2D(24,5,5, subsample=(2,2), activation='relu'))
+model.add(Convolution2D(36,5,5, subsample=(2,2), activation='relu'))
+model.add(Convolution2D(48,5,5, subsample=(2,2), activation='relu'))
+#model.add(MaxPooling2D())
+model.add(Convolution2D(64,3,3, activation='relu'))
+model.add(Convolution2D(64,3,3, activation='relu'))
+#model.add(MaxPooling2D())
 model.add(Flatten())
-model.add(Dense(120))
-model.add(Dense(84))
+model.add(Dense(100))
+model.add(Dense(50))
+model.add(Dense(10))
+
 model.add(Dense(1))
 
 model.compile(optimizer='adam', loss='mse')
+
+model.summary()
+
 model.fit(X_train, y_train, validation_split=0.2, shuffle=True, nb_epoch=5)
 
 model.save('model.h5')
