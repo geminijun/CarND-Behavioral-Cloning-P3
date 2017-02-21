6a8238cee9831f162a2a28b004b2cd759c93ece0
Jun Gan <geminijun@gmail.com>
2017-02-21 07:01:16 +0000

    Finalized code

--- model.py
+++ model.py
@@ -93,13 +93,13 @@ print(history_object.history.keys())
 import matplotlib.pyplot as plt
 
 # ### plot the training and validation loss for each epoch
-plt.plot(history_object.history['loss'])
-plt.plot(history_object.history['val_loss'])
-plt.title('model mean squared error loss')
-plt.ylabel('mean squared error loss')
-plt.xlabel('epoch')
-plt.legend(['training set', 'validation set'], loc='upper right')
-plt.show()
+#plt.plot(history_object.history['loss'])
+#plt.plot(history_object.history['val_loss'])
+#plt.title('model mean squared error loss')
+#plt.ylabel('mean squared error loss')
+#plt.xlabel('epoch')
+#plt.legend(['training set', 'validation set'], loc='upper right')
+#plt.show()
 
 
 model.save('model.h5')
