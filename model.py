4fdaf43955ec21bce8e5204bb0e6ac5dc6e58565
Jun Gan <geminijun@gmail.com>
2017-02-21 03:31:57 +0000

    Temp

--- model.py
+++ model.py
@@ -6,10 +6,12 @@ DATA_DIR = 'data/'
 IMG_DIR = 'IMG/'
 images = []
 measurements = []
-correction = 0.3
+correction = 0.25
+samples = []
 with open(DATA_DIR + 'driving_log.csv', 'r') as csvfile:
   reader = csv.reader(csvfile, delimiter=',')
   for line in reader:
+    samples
     for i in range(3): # 0: center 1: left 2: right
       source_path = line[i]
       tokens = source_path.split('/')
@@ -24,13 +26,16 @@ with open(DATA_DIR + 'driving_log.csv', 'r') as csvfile:
 #    print(images)
 #    exit();
     measurement = float(line[3])
-    measurements.append(measurement)
-    measurements.append(measurement+correction) # left
-    measurements.append(measurement-correction) # right
     flipped_measurement = measurement * -1.0
+    measurements.append(measurement)
     measurements.append(flipped_measurement)
+    measurements.append(measurement+correction) # left
     measurements.append(flipped_measurement+correction)
+    measurements.append(measurement-correction) # right
     measurements.append(flipped_measurement-correction)
+
+from sklearn.model_selection import train_test_split
+train_samples, validation_samples = train_test_split(samples, test_size=0.2)
 
 X_train = np.array(images)
 y_train = np.array(measurements)
@@ -42,7 +47,7 @@ print(X_train.shape)
 
 import keras
 from keras.models import Sequential
-from keras.layers import Flatten, Dense, Lambda
+from keras.layers import Flatten, Dense, Lambda, Dropout
 from keras.layers.convolutional import Convolution2D, Cropping2D
 from keras.layers.pooling import MaxPooling2D
 
@@ -57,9 +62,10 @@ model.add(Convolution2D(64,3,3, activation='relu'))
 model.add(Convolution2D(64,3,3, activation='relu'))
 #model.add(MaxPooling2D())
 model.add(Flatten())
-model.add(Dense(100))
-model.add(Dense(50))
-model.add(Dense(10))
+#model.add(Dropout(0.25))
+model.add(Dense(100, activation='relu'))
+model.add(Dense(50, activation='relu'))
+model.add(Dense(10, activation='relu'))
 
 model.add(Dense(1))
 
@@ -67,6 +73,25 @@ model.compile(optimizer='adam', loss='mse')
 
 model.summary()
 
-model.fit(X_train, y_train, validation_split=0.2, shuffle=True, nb_epoch=10)
+#model.fit(X_train, y_train, validation_split=0.2, shuffle=True, nb_epoch=6)
+
+history_object = model.fit_generator(train_generator, samples_per_epoch =
+    len(train_samples), validation_data =
+    validation_generator,
+    nb_val_samples = len(validation_samples),
+    nb_epoch=5, verbose=1)
+
+### print the keys contained in the history object
+print(history_object.history.keys())
+
+### plot the training and validation loss for each epoch
+plt.plot(history_object.history['loss'])
+plt.plot(history_object.history['val_loss'])
+plt.title('model mean squared error loss')
+plt.ylabel('mean squared error loss')
+plt.xlabel('epoch')
+plt.legend(['training set', 'validation set'], loc='upper right')
+plt.show()
+
 
 model.save('model.h5')
